9dceaeb104c417a4ad636c7324a92edfe0056551
sunhh <sunhh@qingbao.com>
2023-12-30 11:19:18 +0800

    make device api enabled on linux

--- apps/deploy/cpp_module_deploy.cpp
+++ apps/deploy/cpp_module_deploy.cpp
@@ -11,6 +11,13 @@
 #include <algorithm>
 #include <vta/runtime/runtime.h>
 
+#if defined(_WIN32)
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
+
+
 void matrix_multiply(std::string model_path){
     tvm::runtime::Module mod_dylib =
             tvm::runtime::Module::LoadFromFile(model_path.c_str());
@@ -22,9 +29,19 @@ void matrix_multiply(std::string model_path){
     }else{
         LOG(INFO) << "got main func";
     }
+    tvm::Device _device(DLDevice{kDLExtDev, 0});
+    tvm::runtime::NDArray A_nd = tvm::runtime::NDArray::Empty({1,2}, tvm::DataType::Int(8),
+                                                              _device);
+}
+void* load_vta_driver(std::string vta_driver_dso){
+    auto lib_handle_ = dlopen(vta_driver_dso.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+    ICHECK(lib_handle_ != nullptr) << "Failed to load dynamic shared library " << vta_driver_dso << " "
+                                   << dlerror();
+    return lib_handle_;
 
 }
 int main(int argc, char** argv){
-    matrix_multiply(argv[1]);
+    auto driver_handle = load_vta_driver(argv[1]);
+    matrix_multiply(argv[2]);
     return 0;
 }
