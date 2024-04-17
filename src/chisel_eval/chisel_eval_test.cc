7d873ef98f29b6765a135ff754f9ac97129e2696
sunhh <sunhh@qingbao.com>
2024-04-17 13:26:57 +0800

    visit correct address offset in axilite register

--- src/chisel_eval/chisel_eval_test.cc
+++ src/chisel_eval/chisel_eval_test.cc
@@ -20,11 +20,11 @@ int main(int argc, char** argv)
     auto f_write = n.GetFunction("WriteReg");
     auto f_read = n.GetFunction("ReadReg");
     int ret = f2(10);
-    std::this_thread::sleep_for(std::chrono::seconds (10));
+    std::this_thread::sleep_for(std::chrono::seconds (5));
     if (ret){
         printf("begin to cal it");
-        f_write(10, 5);
-        int val = f_read(10);
+        f_write(0x18, 5);
+        int val = f_read(0x18);
         LOG(INFO) << "val : " << val;
     }
 //    tvm::runtime::NDArray ret_arr = f3(15);
